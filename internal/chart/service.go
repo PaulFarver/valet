2f9d7e43c6d753ec40f01f1739ee520652748cd0
Paul Farver <paulfarver@gmail.com>
2022-02-20 16:06:33 +0100

    Remove unecessary info from chart indexresponse

--- internal/chart/service.go
+++ internal/chart/service.go
@@ -9,49 +9,12 @@ import (
 )
 
 type IndexResponse struct {
-	APIVersion APIVersion             `yaml:"apiVersion"`
-	Entries    map[string][]ChartInfo `yaml:"entries"`
-	Generated  string                 `yaml:"generated"`
+	Entries map[string][]ChartInfo `yaml:"entries"`
 }
 
 type ChartInfo struct {
-	APIVersion   APIVersion   `yaml:"apiVersion"`
-	AppVersion   *string      `yaml:"appVersion,omitempty"`
-	Created      string       `yaml:"created"`
-	Description  string       `yaml:"description"`
-	Digest       string       `yaml:"digest"`
-	Home         *string      `yaml:"home,omitempty"`
-	Icon         *string      `yaml:"icon,omitempty"`
-	Maintainers  []Maintainer `yaml:"maintainers"`
-	Name         string       `yaml:"name"`
-	Sources      []string     `yaml:"sources"`
-	Urls         []string     `yaml:"urls"`
-	Version      string       `yaml:"version"`
-	Deprecated   *bool        `yaml:"deprecated,omitempty"`
-	Keywords     []string     `yaml:"keywords"`
-	KubeVersion  *string      `yaml:"kubeVersion,omitempty"`
-	Dependencies []Dependency `yaml:"dependencies"`
+	Version string `yaml:"version"`
 }
-
-type Dependency struct {
-	Name       string  `yaml:"name"`
-	Repository string  `yaml:"repository"`
-	Version    string  `yaml:"version"`
-	Alias      *string `yaml:"alias,omitempty"`
-}
-
-type Maintainer struct {
-	Email *string `yaml:"email"`
-	Name  string  `yaml:"name"`
-	URL   *string `yaml:"url,omitempty"`
-}
-
-type APIVersion string
-
-const (
-	V1 APIVersion = "v1"
-	V2 APIVersion = "v2"
-)
 
 type Service interface {
 	ListVersions(ctx context.Context, repository, chart string) ([]string, error)
